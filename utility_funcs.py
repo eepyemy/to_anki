3844fd4a0c36149458c71a3dab765dca66e578f8
eepyemy <emy.ict@proton.me>
2023-12-08 09:28:44 +0100

    migrated utility funcs

---
+++ utility_funcs.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def str_to_date(str):
+  return datetime.strptime(str, '%Y-%m-%dT%H:%M:%SZ')
+
+def ms_to_date(ms):
+  return datetime.fromtimestamp(int(ms))
+
+def ms_to_str(ms):
+  return date_to_str(ms_to_date(ms))
+
+def date_to_ms(date):
+  return date.timestamp()
+
+def date_to_str(date, ms_timestamp=True):
+  return date.strftime("%Y-%m-%dT%H:%M:%SZ")
